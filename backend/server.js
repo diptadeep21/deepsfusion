881a1a4781630387207ec2130286c3654afb29f7
Diptadeep Sarkar <diptadeep21@gmail.com>
2026-03-11 15:48:18 +0530

    ignore env files

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,42 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+
 import userRouter from './routes/userRoute.js'
 import productRouter from './routes/productRoute.js'
 import orderRouter from './routes/orderRoute.js'
 import cartRouter from './routes/cartRoute.js'
 
-// App Config
+// app config
 const app = express()
 const port = process.env.PORT || 4000
+
+// connect database
 connectDB()
 connectCloudinary()
 
 // middlewares
 app.use(express.json())
-app.use(cors())
 
-// api endpoints
+app.use(cors({
+  origin: "*",
+  credentials: true
+}))
+
+// api routes
 app.use('/api/user', userRouter)
 app.use('/api/product', productRouter)
 app.use('/api/order', orderRouter)
 app.use('/api/cart', cartRouter)
 
-
-
+// test route
 app.get('/', (req,res)=>{
-    res.send("API Working")
+  res.send("API Working")
 })
 
-app.listen(port, ()=> console.log('Server started on PORT: '+ port))
+app.listen(port, ()=>{
+  console.log("Server started on PORT: " + port)
+})
